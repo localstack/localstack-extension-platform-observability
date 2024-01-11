60d628f1bbe2cb8c7207a4d8cadbc594d4753422
Thomas Rausch <thomas@thrau.at>
2024-01-11 16:20:07 +0100

    re-organize metric endpoints and trace locations (#2)

--- platform_observability/endpoint.py
+++ platform_observability/endpoint.py
@@ -1,7 +1,7 @@
 import time
 
-from localstack.http import route
-from requests import Request
+from localstack.http import Request, route
+from werkzeug.exceptions import NotFound
 
 from .instruments.core import AggregatingInstrument, Instrument, ListCollector
 from .instruments.sns import TopicStatistics
@@ -9,34 +9,33 @@ from .instruments.sqs import QueueStatistics
 
 
 class MetricsEndpoint:
-    def __init__(
-        self,
-        instruments: dict[str, Instrument],
-        queue_statistics: QueueStatistics,
-        topic_statistics: TopicStatistics,
-    ):
+    def __init__(self, instruments: dict[str, Instrument]):
         self.instruments = instruments
-        self.topic_statistics = topic_statistics
-        self.queue_statistics = queue_statistics
 
-    @route("/_extension/metrics/all")
-    def get_all(self, request: Request):
-        timestamp = time.time()
-        aggregator = AggregatingInstrument(self.instruments)
+    @route("/_extension/observability/metrics")
+    def get_metrics(self, request: Request):
+        instrument_filter = request.args.getlist("instrument")
+
+        instruments = self.instruments
+        if instrument_filter:
+            instruments = {k: v for k, v in instruments.items() if v in instrument_filter}
+
+        aggregator = AggregatingInstrument(instruments, flatten=False)
         collector = ListCollector()
         aggregator.measure_and_report(collector)
+
         record = collector.records[0]
-        record["timestamp"] = timestamp
+        record["timestamp"] = time.time()
         return record
 
-    @route("/_extension/metrics/sqs")
-    def get_sqs(self, request: Request):
+    @route("/_extension/observability/metrics/<instrument>")
+    def get_metrics_for_instrument(self, request: Request, instrument: str):
         collector = ListCollector()
-        self.queue_statistics.measure_and_report(collector)
-        return {"queues": collector.records}
 
-    @route("/_extension/metrics/sns")
-    def get_sns(self, request: Request):
-        collector = ListCollector()
-        self.topic_statistics.measure_and_report(collector)
-        return {"topics": collector.records}
+        try:
+            instrument_obj = self.instruments[instrument]
+        except KeyError:
+            raise NotFound(f"unknown instrument {instrument}")
+
+        instrument_obj.measure_and_report(collector)
+        return {"timestamp": time.time(), instrument: collector.records}
